7bf7ea7e6b5942102f4e68f31eb26c09f511479a
silenkovkonstantin <silenkov@inbox.ru>
2022-12-26 20:38:32 +0500

    Задание 3.2

--- Program.cs
+++ Program.cs
@@ -5,10 +5,17 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        string myName;
-        myName = "Kostya";
+        const string myName = "Kostya";
 
         Console.WriteLine(myName);
+        Console.WriteLine("Привет, \n мир!");
+        Console.WriteLine("\u0023");
+        Console.WriteLine(true);
+        Console.WriteLine(false);
+        Console.WriteLine(5);
+        Console.WriteLine(0x0A);
+        Console.WriteLine(0b11);
+        Console.WriteLine(5.5);
 
         Console.ReadKey();
     }
